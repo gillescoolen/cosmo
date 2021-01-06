055ca632880dd7789cec5184b0499bb05b7bc1f8
Gilles <gilles@coolen.dev>
2021-01-06 14:33:34 +0100

    Fix setup

--- Cosmo.Domain/Context/ApplicationDbContext.cs
+++ Cosmo.Domain/Context/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Cosmo.Data
+namespace Cosmo.Presentation.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- Cosmo.Presentation/Startup.cs
+++ Cosmo.Presentation/Startup.cs
@@ -8,12 +8,12 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using Cosmo.Data;
+using Cosmo.Presentation.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Cosmo
+namespace Cosmo.Presentation
 {
     public class Startup
     {
@@ -24,7 +24,6 @@ namespace Cosmo
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -35,7 +34,6 @@ namespace Cosmo
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -46,7 +44,6 @@ namespace Cosmo
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
